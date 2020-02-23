6fa1f9e2677c3c3e6a62979d28d4ce9918cd5a1a
Robert Kerr <rob@roberts-mbp.attlocal.net>
2020-02-23 16:59:44 -0600

    refactor test

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -5,35 +5,29 @@ use PHPUnit\Framework\TestCase;
 
 class PrimeFactorsTest extends TestCase
 {
-    /** @test */
-    function it_generates_prime_factors_for_1()
+    /**
+     * @test
+     * @dataProvider factors
+     * @param $number
+     * @param $expectedResult
+     */
+    function it_generates_prime_factors($number, $expectedResult)
     {
         $factors = new PrimeFactors();
 
-        $this->assertEquals([], $factors->generate(1));
+        $this->assertEquals($expectedResult, $factors->generate($number));
     }
 
-    /** @test */
-    function it_generates_prime_factors_for_2()
+    function factors()
     {
-        $factors = new PrimeFactors();
-
-        $this->assertEquals([2], $factors->generate(2));
-    }
-
-    /** @test */
-    function it_generates_prime_factors_for_3()
-    {
-        $factors = new PrimeFactors();
-
-        $this->assertEquals([3], $factors->generate(3));
-    }
-
-    /** @test */
-    function it_generates_prime_factors_for_4()
-    {
-        $factors = new PrimeFactors();
-
-        $this->assertEquals([2,2], $factors->generate(4));
+        return [
+            [1, []],
+            [2, [2]],
+            [3, [3]],
+            [4, [2,2]],
+            [5, [5]],
+            [6, [2,3]],
+            [8, [2,2,2]],
+        ];
     }
 }
