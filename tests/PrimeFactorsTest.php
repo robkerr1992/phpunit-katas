c3b4ae5b4c39c729ef72befd9fcf69168407ee75
Robert Kerr <rob@roberts-mbp.attlocal.net>
2020-02-23 16:55:04 -0600

    test 4

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -28,4 +28,12 @@ class PrimeFactorsTest extends TestCase
 
         $this->assertEquals([3], $factors->generate(3));
     }
+
+    /** @test */
+    function it_generates_prime_factors_for_4()
+    {
+        $factors = new PrimeFactors();
+
+        $this->assertEquals([2,2], $factors->generate(4));
+    }
 }
